a12335457825e677bc75caedd2de55c90352eb87
Juan <juanfelipegomez.c@gmail.com>
2019-08-13 20:10:15 +0200

    Add more data loaders

--- src/js/dataLoader.js
+++ src/js/dataLoader.js
@@ -1,10 +1,12 @@
-import { csv } from 'd3';
+import { csv, json } from 'd3';
 
 export const getActivities = () => csv('src/data/activities.csv');
 export const getParticipations = () => csv('src/data/participations.csv');
 
 export const getAreaDomainPerCountry = () =>
   csv('src/data/area_domain_per_country.csv');
+export const getAreas = () => json('src/data/areas_without_patents.json');
+export const getProjects = () => json('src/data/projects.json');
 
 export const getAllData = () =>
   new Promise((resolve, reject) =>
